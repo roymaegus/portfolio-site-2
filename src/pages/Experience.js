31a3d03c51e13d8fbc5807756f34ea12ec08491a
Roy Maegus <wolfmagus0@gmail.com>
2023-09-07 23:07:16 -0400

    second react portfolio

---
+++ src/pages/Experience.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import {
+//   VerticalTimeline,
+//   VerticalTimelineElement,
+// } from "react-vertical-timeline-component";
+// import "react-vertical-timeline-component/style.min.css";
+// import SchoolIcon from "@material-ui/icons/School";
+//import WorkIcon from "@material-ui/icons/Work";
+
+function Experience() {
+  return (
+    <div className="experience">
+      
+    </div>
+  );
+}
+
+export default Experience;
